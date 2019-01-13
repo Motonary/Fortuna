fcfd8337932c8c2a0d3a4ceb908fa8978b696e7b
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-01-14 00:18:38 +0900

    add Button & TextBox component

---
+++ client/src/components/atoms/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+function buttonFactory(type: string) {
+  return ({ children, className, ...props }: any) => (
+    <button className={['button', type, className].join(' ')} {...props}>
+      {children}
+    </button>
+  )
+}
+
+export const Button = buttonFactory('default')
+export const PrimaryButton = buttonFactory('primary')
+
+export default Button
---
+++ client/src/components/atoms/TextBox/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+interface TextBoxProps {
+  className: string
+}
+
+const TextBox: React.SFC<TextBoxProps> = ({ className, ...props }: any) => (
+  <input type="text" className={['textbox', className].join(' ')} {...props} />
+)
+
+export default TextBox
