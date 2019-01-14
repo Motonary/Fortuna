67c77a8eed739f3488b71845a99ffd8e29a01ec5
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-01-14 20:57:10 +0900

    Create huskyrc

--- client/src/components/atoms/demo/demo.tsx
+++ client/src/components/atoms/demo/demo.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 // import * as style from './style.scss'
 
-const Demo: React.SFC<{}> = () => <h1>Hello, Motonary!</h1>
+const Demo: React.SFC<{}> = () => <h1>Hello, Motonary!!</h1>
 
 export default Demo
