4be98ad2661c1847d07940993aa3c8667bab89ec
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-01-18 05:04:03 +0900

    set type

--- client/src/components/utils/auth.tsx
+++ client/src/components/utils/auth.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 
-const Auth = props => (props.currentUser.isLoggedIn ? props.children : <Redirect to={'/'} />)
+const Auth = (props: any) => (props.currentUser.isLoggedIn ? props.children : <Redirect to={'/'} />)
 
 export default Auth
