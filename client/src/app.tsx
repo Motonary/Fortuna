53e0aa5c85e2a83219e7907442306755c6c52e0a
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-01-19 17:51:24 +0900

    remove redux-promise

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import promise from 'redux-promise'
 import reducers from './reducers'
 
 import Hello from './components/Hello'
 import Auth from './components/utils/auth'
 
-const createStoreWithMiddleware: any = applyMiddleware(promise)(createStore)
+// const createStoreWithMiddleware: any = applyMiddleware(promise)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
