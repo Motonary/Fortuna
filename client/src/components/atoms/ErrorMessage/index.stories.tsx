fc535ea789a15d4fc01f8f1fa0c757540f01b759
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-01-19 18:06:34 +0900

    remove own ErrorMessage

--- client/src/components/atoms/ErrorMessage/index.stories.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-import { storiesOf } from '@storybook/react'
-import ErrorMessage from './index'
-
-storiesOf('ErrorMessage', module).add('デフォルト', () => <ErrorMessage name="email" />)
