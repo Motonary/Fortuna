d09c2b552266b8ee5d4218e78bcc9bd082d0b457
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-01-21 23:02:24 +0900

    Successfully connect to database

--- database/database.go
+++ database/database.go
@@ -1,32 +1,43 @@
-package database
-
-import "os"
+package main
 
 import (
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/inflection"
 )
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"size:255"`
+	Email    string `gorm:"size:255"`
+	Password string `gorm:"size:255"`
+}
 
 func main() {
 	db := gormConnect()
 	defer db.Close()
+
+	db.AutoMigrate(&User{})
+	// db.Create(&User{Name: "test", Email: "ririco722@motonary.com", Password: "password"})
 }
 
 func gormConnect() *gorm.DB {
 	DBMS := "mysql"
-	USER := "fortuna"
+	USER := "Motonary"
 	PASS := os.Getenv("DB_PASS")
 	PROTOCOL := "" // 必要？
 	DBNAME := "fortuna"
 	OPTION := "charset=utf8&parseTime=True&loc=Local" // enable time.Time
 
-	CONNECT = USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?" + OPTION
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?" + OPTION
 	db, err := gorm.Open(DBMS, CONNECT)
 
 	if err != nil {
 		panic("failed to connect database")
 	}
+
 	return db
 }
