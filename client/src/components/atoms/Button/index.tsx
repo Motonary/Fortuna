a9372e7321d6bfb35fa212a02b1e26de89fde62c
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-01-24 22:06:09 +0900

    Delete fuctory

--- client/src/components/atoms/Button/index.tsx
+++ client/src/components/atoms/Button/index.tsx
@@ -1,24 +1,13 @@
 import * as React from 'react'
 import * as styles from './style.css'
 
-function buttonFactory(type: 'normal' | 'primary' | 'warning') {
-  return ({ children, className, ...props }: any) => (
-    <button className={[styles.button, styles[type], className].join(' ')}>{children}</button>
-  )
+interface ButtonProps {
+  type: 'normal' | 'primary' | 'warning'
+  label: string
 }
 
-export const Button = buttonFactory('normal')
-export const PrimaryButton = buttonFactory('primary')
+const Button: React.SFC<ButtonProps> = ({ type, label }) => (
+  <button className={[styles.button, styles[type]].join(' ')}>{label}</button>
+)
 
 export default Button
-
-// interface ButtonProps {
-//   type: 'normal' | 'primary' | 'warning'
-//   label: string
-// }
-
-// const Button: React.SFC<ButtonProps> = ({ type, label }) => (
-//   <button className={[styles.button, styles[type]].join(' ')}>{label}</button>
-// )
-
-// export default Button
