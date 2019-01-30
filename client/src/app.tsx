d6b995d62faf0896ffd6030cae98ead552d41704
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-01-30 22:56:56 +0900

    Uncomment thunk

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -2,17 +2,17 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import reducers from './reducers'
-// import thunk from 'redux-thunk'
+import thunk from 'redux-thunk'
 
 import Hello from './components/Hello'
 import Auth from './components/utils/auth'
 
-// const store = createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <Router>
       <Switch>
         <Route exact path="/" component={Hello} />
