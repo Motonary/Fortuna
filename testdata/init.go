1a3689266c6f3677ec33c784f6480edcd42a69e8
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-02-01 20:48:29 +0900

    add testdata

---
+++ testdata/init.go
@@ -0,0 +1,76 @@
+package testdata
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	yaml "gopkg.in/yaml.v2"
+
+	"github.com/motonary/Fortuna/entity"
+)
+
+type Data struct {
+  Users []entity.User `yaml:"users"`
+}
+
+var (
+	DB *gorm.DB
+	seed Data
+)
+
+func init() {
+	yml := loadConfig(getConfigFile())
+	_ = yaml.Unmarshal(yml, &seed)
+
+	DB = connect()
+}
+
+func connect() *gorm.DB {
+	DBMS := "mysql"
+	USER := "Motonary"
+	PASS := "ririco722"
+	PROTOCOL := ""
+	DBNAME := "fortuna_test"
+	OPTION := "charset=utf8&parseTime=True&loc=Local" // enable time.Time
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?" + OPTION
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic("database couldn't be opend\n")
+	}
+
+	return db
+}
+
+func ResetDB() *gorm.DB {
+	err := runMakeSeeds(DB, seed)
+	if err != nil {
+		panic("making seeds couldn't run through\n")
+	}
+	return DB
+}
+
+func getConfigFile() string {
+	if _, err := os.Stat("config/testdatas.yml"); err == nil {
+		return "config/testdatas.yml"
+	}
+	return "../config/testdatas.yml"
+}
+
+func loadConfig(file_path string) []byte {
+	yml, err := ioutil.ReadFile(file_path)
+	if err != nil {
+		panic("couldn't read testdata config\n")
+	}
+	return yml
+}
+
+func runMakeSeeds(DB *gorm.DB, seed Data) error {
+	for _, user := range seed.Users {
+		DB.Create(user)
+	}
+
+	return nil
+}
