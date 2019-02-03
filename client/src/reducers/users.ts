829759257f00b9bb52dc849e3fc525afe2b9ac94
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-02-03 15:12:14 +0900

    small fix

--- client/src/reducers/users.ts
+++ client/src/reducers/users.ts
@@ -3,7 +3,7 @@ import { User } from '../constants/static-types'
 import { SwitchIsLoadingAction, CurrentUserAction } from '../actions/users'
 import { ReduxAPIStruct, defaultSet } from './reducer-type'
 
-export function CurrentUser(state: ReduxAPIStruct<User> = defaultSet(), action: CurrentUserAction) {
+export function currentUser(state: ReduxAPIStruct<User> = defaultSet(), action: CurrentUserAction) {
   switch (action.type) {
     case actionTypes.SET_CURRENT_USER:
       if ('currentUser' in action.payload) {
