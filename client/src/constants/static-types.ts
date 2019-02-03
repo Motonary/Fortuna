9e7390d8d4b84c7c2fb013ee903ba6f3cc802587
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-02-03 19:31:56 +0900

    Add 5xx status code

--- client/src/constants/static-types.ts
+++ client/src/constants/static-types.ts
@@ -14,6 +14,6 @@ export interface User {
 
 export type StatusCodeSuccess = 200 | 204
 
-export type StatusCodeFailure = 400 | 401 | 403 | 404 | 422
+export type StatusCodeFailure = 400 | 401 | 403 | 404 | 422 | 500 | 503
 
 export type StatusCode = StatusCodeSuccess | StatusCodeFailure
