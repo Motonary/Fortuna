e1ea43db9bb764f88975f96c2aab8d54eb825520
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-02-06 23:08:04 +0900

    modify gopath

---
+++ api/v1/routes_test.go
@@ -0,0 +1,94 @@
+package v1_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/jwtauth"
+
+	api "github.com/motonary/Fortuna/api/v1"
+	"github.com/motonary/Fortuna/entity"
+)
+
+var (
+	router      *chi.Mux
+	testUser    *entity.User
+	testBody    []byte
+	tokenAuth   *jwtauth.JWTAuth
+	tokenString string
+)
+
+func TestUnauthorizedRequestHandle(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/users/1", nil)
+
+	api.Router().ServeHTTP(w, r)
+	rw := w.Result()
+	defer rw.Body.Close()
+
+	t.Logf("responsed status code : %d\n\n", rw.StatusCode)
+
+	if rw.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("unexpected status code : %d\n\n", rw.StatusCode)
+	}
+}
+
+func TestAuthorizeRequestHandle(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/users", bytes.NewBuffer(testBody))
+
+	api.Router().ServeHTTP(w, r)
+	rw := w.Result()
+	defer rw.Body.Close()
+
+	var responsedBody Response
+	responsedByteBody, _ := ioutil.ReadAll(rw.Body)
+	json.Unmarshal(responsedByteBody, &responsedBody)
+	tokenString = responsedBody.Token
+
+	t.Logf("%v\n", tokenString)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest("GET", "/auth", nil)
+	r.Header.Set("Authorization", "Bearer "+tokenString)
+
+	api.Router().ServeHTTP(w, r)
+	res := w.Result()
+	defer res.Body.Close()
+
+	t.Logf("responsed status code : %d\n\n", rw.StatusCode)
+
+	if rw.StatusCode != http.StatusOK {
+		t.Fatalf("unexpected status code : %d\n\n", rw.StatusCode)
+	}
+}
+
+func setup() {
+	println("setup")
+
+	testUser = entity.NewUser(0, "ririco", "ririco@example.com", "password")
+	testBody, _ = json.Marshal(testUser)
+
+	tokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
+
+	println(testUser)
+}
+
+func teardown() {
+	println("teardown")
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	ret := m.Run()
+	if ret == 0 {
+		teardown()
+	}
+	os.Exit(ret)
+}
---
+++ api/v1/user_handler_test.go
@@ -0,0 +1,87 @@
+package v1_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	api "github.com/motonary/Fortuna/api/v1"
+	"github.com/motonary/Fortuna/entity"
+)
+
+type Response struct {
+	Status int          `json:"status"`
+	User   *entity.User `json:"user,omitempty"`
+	Token  string       `json:"token,omitempty"`
+}
+
+func TestCreateUserHandler(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/users", bytes.NewBuffer(testBody))
+
+	api.Router().ServeHTTP(w, r)
+
+	rw := w.Result()
+	defer rw.Body.Close()
+
+	if rw.StatusCode != http.StatusOK {
+		t.Fatalf("unexpected status code : %d\n\n", rw.StatusCode)
+	}
+
+	var response Response
+	bytes, _ := ioutil.ReadAll(rw.Body)
+	json.Unmarshal(bytes, &response)
+
+	if response.User.Name != testUser.Name {
+		t.Fatalf("response data is unexpected : %v\n\n", response.User)
+	}
+}
+
+func TestGetUserHandlerResponse(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/users/2", nil)
+	r.Header.Set("Authorization", "Bearer "+tokenString)
+
+	api.Router().ServeHTTP(w, r)
+
+	rw := w.Result()
+	defer rw.Body.Close()
+
+	if rw.StatusCode != http.StatusOK {
+		t.Fatalf("unexpected status code : %d\n\n", rw.StatusCode)
+	}
+}
+
+// updateのサーバとクライアントのインターフェースが未定のためペンディング
+// func TestUpdatetUserHandlerResponse(t *testing.T) {
+// 	w := httptest.NewRecorder()
+// 	r := httptest.NewRequest("PUT", "/users/2", nil)
+// 	r.Header.Set("Authorization", "Bearer "+tokenString)
+
+// 	api.Router().ServeHTTP(w, r)
+
+// 	rw := w.Result()
+// 	defer rw.Body.Close()
+
+// 	if rw.StatusCode != http.StatusOK {
+// 		t.Fatalf("unexpected status code : %d\n\n", rw.StatusCode)
+// 	}
+// }
+
+func TestDeleteUserHandlerResponse(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("DELETE", "/users/2", nil)
+	r.Header.Set("Authorization", "Bearer "+tokenString)
+
+	api.Router().ServeHTTP(w, r)
+
+	rw := w.Result()
+	defer rw.Body.Close()
+
+	if rw.StatusCode != http.StatusOK {
+		t.Fatalf("unexpected status code : %d\n\n", rw.StatusCode)
+	}
+}
