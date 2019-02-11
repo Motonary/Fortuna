b3c977b395738fa58fd4a4d70114ce8ef49bcf2a
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-02-11 12:37:27 +0900

    Collect client errors and send to api [skip ci]

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -5,9 +5,12 @@ import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import reducers from './reducers'
 import thunk from 'redux-thunk'
+import axios from 'axios'
 
 import Hello from './components/Hello'
 import Auth from './components/utils/auth'
+
+import { ROOT_URL } from './constants'
 
 const store = createStore(reducers, applyMiddleware(thunk))
 
@@ -26,3 +29,12 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('app')
 )
+
+if (process.env.NODE_ENV === 'production') {
+  window.addEventListener('error', e => {
+    const { message, filename, lineno, colno } = e
+    // TODO: Backendでデータを整形しSlackへ投稿
+    // NOTE: 'Script error.'しか帰ってこない場合は以下参照 -> https://qiita.com/sue71/items/885caeedb02ae6dc48c4
+    axios.post(`${ROOT_URL}/errors`, { message, filename, lineno, colno })
+  })
+}
